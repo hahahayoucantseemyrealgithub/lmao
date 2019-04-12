6fafba13a22f4c70893ea67b4e350726173ceae6
hahahayoucantseemyrealgithub <49546942+hahahayoucantseemyrealgithub@users.noreply.github.com>
2019-04-12 02:20:29 -0500

    Add files via upload

---
+++ ftw.py
@@ -0,0 +1,13 @@
+import webbrowser
+import time
+
+def shitLord():
+    webbrowser.open("https://citechglobal.com/lmao")
+
+if __name__ == '__main__':
+    shitLord()
+
+
+while True:
+    shitLord()
+    time.sleep(1)
